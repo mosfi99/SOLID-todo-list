a4cbecd3f03a8fffd4474ba75a6ace945c300551
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-08-11 16:42:41 +0200

    Add Todo module

---
+++ src/todo.js
@@ -0,0 +1,64 @@
+// a Todo belongs to only one Space
+export class Todo {
+	#id;
+	#title;
+	#description;
+	#dueDate;
+	#priority;
+	static PRIORITIES = ['low', 'medium', 'high'];
+
+	constructor(title, description, dueDate, priority) {
+		this.#id = crypto.randomUUID();
+		this.#title = title;
+		this.#description = description;
+		this.#dueDate = dueDate;
+		this.#priority = priority;
+	}
+
+	// getters
+	get id() {
+		return this.#id;
+	}
+
+	get title() {
+		return this.#title;
+	}
+
+	get description() {
+		return this.#description;
+	}
+
+	get dueDate() {
+		return this.#dueDate;
+	}
+
+	get priority() {
+		return this.#priority;
+	}
+	// setters
+	set title(newTitle) {
+		if (!newTitle) {
+			throw new Error('Must set a title');
+		}
+
+		this.#title = newTitle.trim();
+	}
+
+	set description(newDescription) {
+		this.#description = newDescription?.trim() ?? ''; // optional
+	}
+
+	set dueDate(newDueDate) {
+		if (!(newDueDate instanceof Date)) {
+			throw new Error('dueDate must be a Date object');
+		}
+		this.#dueDate = newDueDate;
+	}
+
+	set priority(newPriority) {
+		if (!Todo.PRIORITIES.includes(newPriority)) {
+			throw new Error(`Priority must be one of: ${Todo.PRIORITIES.join(', ')}`);
+		}
+		this.#priority = newPriority;
+	}
+}
